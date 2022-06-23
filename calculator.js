99268003c5846b636b305dc643b0c41f1dfe8fb3
kachically02 <kachically89@gmail.com>
2022-06-24 10:16:24 +1300

    implement simple arithmetic calculator

---
+++ calculator.js
@@ -0,0 +1,22 @@
+
+
+function add(n1, n2){
+    return n1 + n2;
+}
+console.log(add(2, 2))
+
+function ply(n1, n2) {
+    return n1 * n2;
+}
+
+function dv(n1, n2) {
+    return n1 / n2;
+}
+
+function sub(n1, n2) {
+    return n1 - n2;
+}
+
+function calc(n1, n2, op) {
+    return open(n1, n2);
+}
